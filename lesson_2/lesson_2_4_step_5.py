60e4e5aba3b0669b2b8145847fd42e66749f5836
victor-kozlovsky <victor.kozlovsky@gmail.com>
2019-12-04 15:55:38 +0300

    2.4 Selenium Waits (Implicit Waits)

---
+++ lesson_2/lesson_2_4_step_5.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+
+try:
+    browser = webdriver.Chrome()
+    # говорим WebDriver искать каждый элемент в течение 5 секунд
+    browser.implicitly_wait(5)
+    browser.get("http://suninjuly.github.io/wait1.html")
+    
+    button = browser.find_element_by_id("verify")
+    button.click()
+    message = browser.find_element_by_id("verify_message")
+    
+    assert "successful" in message.text
+    
+finally:
+    # смотрим 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
